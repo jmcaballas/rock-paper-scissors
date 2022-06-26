662998b475f0f60a802b0e6e6268274cc4f7c35f
Joseph Matthew Caballas <jmcaballas@gmail.com>
2022-06-26 23:32:05 +0800

    Translate computer random number to computer choice

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
 function computerPlay() {
-    return Math.floor(Math.random() * (3)) + 1;
+    let randomNumber = Math.floor(Math.random() * (3)) + 1;
+    let computerChoice;
+    if (randomNumber === 1) {
+        computerChoice = 'ROCK';
+    } else if (randomNumber === 2) {
+        computerChoice = 'PAPER';
+    } else if (randomNumber === 3) {
+        computerChoice = 'SCISSORS';
+    }
+    return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
